038ea8692d1669bfc398c7f250afca6cead4c166
rehan <rehanmir30997@gmail.com>
2021-05-17 14:41:57 +0500

    updated

--- app/src/main/java/com/example/cupid/MessagesFragments/messages_All.java
+++ app/src/main/java/com/example/cupid/MessagesFragments/messages_All.java
@@ -8,7 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.example.cupid.Activities.ChannelActivity;
+import com.example.cupid.databinding.FragmentMessagesAllBinding;
+
+import io.getstream.chat.android.client.ChatClient;
+import io.getstream.chat.android.client.api.models.FilterObject;
+import io.getstream.chat.android.client.models.Filters;
+import io.getstream.chat.android.client.models.User;
+import io.getstream.chat.android.livedata.ChatDomain;
+import io.getstream.chat.android.ui.channel.list.ChannelListView;
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel;
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModelBinding;
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory;
+
+import static java.util.Collections.singletonList;
+
+
 import com.example.cupid.R;
+import com.example.cupid.databinding.FragmentMessagesAllBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +86,48 @@ public class messages_All extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages__all, container, false);
+        View v=inflater.inflate(R.layout.fragment_messages__all, container, false);
+
+        FragmentMessagesAllBinding binding = FragmentMessagesAllBinding.inflate(getLayoutInflater());
+        v=(binding.getRoot());
+        ChatClient client = new ChatClient.Builder("b67pax5b2wdq", getContext()).build();
+        new ChatDomain.Builder(client, getContext()).build();
+
+        // Step 2 - Authenticate and connect the user
+        User user = new User();
+        user.setId("tutorial-droid");
+        user.getExtraData().put("name", "Tutorial Droid");
+        user.getExtraData().put("image", "https://bit.ly/2TIt8NR");
+
+        client.connectUser(
+                user,
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
+        ).enqueue();
+
+        // Step 3 - Set the channel list filter and order
+        // This can be read as requiring only channels whose "type" is "messaging" AND
+        // whose "members" include our "user.id"
+        FilterObject filter = Filters.and(
+                Filters.eq("type", "messaging"),
+                Filters.in("members", singletonList(user.getId()))
+        );
+
+        ChannelListViewModelFactory factory = new ChannelListViewModelFactory(
+                filter,
+                ChannelListViewModel.DEFAULT_SORT
+        );
+
+        ChannelListViewModel channelsViewModel =
+                new ViewModelProvider(this, factory).get(ChannelListViewModel.class);
+
+        // Step 4 - Connect the ChannelListViewModel to the ChannelListView, loose
+        //          coupling makes it easy to customize
+        ChannelListViewModelBinding.bind(channelsViewModel, binding.channelListView, this);
+        binding.channelListView.setChannelItemClickListener(
+                channel -> startActivity(ChannelActivity.newIntent(getContext(), channel))
+        );
+
+
+        return v;
     }
 }
