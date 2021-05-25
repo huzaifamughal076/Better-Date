640616c290b0a4b9f46cae7c53094ebee8e52017
rehan <rehanmir30997@gmail.com>
2021-05-25 15:59:21 +0500

    updated

--- app/src/main/java/com/example/cupid/Fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/cupid/Fragments/DiscoverFragment.java
@@ -89,6 +89,7 @@ public class DiscoverFragment extends Fragment {
     int counter = 0;
 
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
