aedb71ee0eee51b31ea210b64b94e7866703ba20
Huzaifa Mughal <huzaifamughal076@gmail.com>
2021-05-27 13:18:04 +0500

    new

--- app/src/main/java/com/example/cupid/Activities/Profile_Preferences.java
+++ app/src/main/java/com/example/cupid/Activities/Profile_Preferences.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.cupid.Fragments.ProfileFragment;
@@ -13,6 +15,10 @@ import com.example.cupid.R;
 
 public class Profile_Preferences extends AppCompatActivity {
 
+ImageView back_arrow;
+TextView from_age , to_age;
+
+RelativeLayout height_layout;
 
 
     @Override
@@ -20,10 +26,34 @@ public class Profile_Preferences extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile__preferences);
 
+        back_arrow = findViewById(R.id.back_arrow);
+        height_layout = findViewById(R.id.height);
+
+//        from_age = findViewById(R.id.from_ages);
+//        to_age = findViewById(R.id.to_ages);
+//
+//        String st = getIntent().getStringExtra("start_age");
+//        String en = getIntent().getStringExtra("end_age");
+//
+//        from_age.setText(st);
+//        to_age.setText(en);
 
 
+        height_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Height.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
-
+        back_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
     }
@@ -31,12 +61,14 @@ public class Profile_Preferences extends AppCompatActivity {
     public void gender(View view) {
         Intent i = new Intent(getApplicationContext(),Preferences_Gender.class);
         startActivity(i);
+        finish();
 
     }
 
     public void bodytype(View view) {
         Intent i = new Intent(getApplicationContext(),BodyType.class);
         startActivity(i);
+        finish();
 
 
     }
@@ -45,6 +77,7 @@ public class Profile_Preferences extends AppCompatActivity {
 
         Intent i = new Intent(getApplicationContext(),PreferencesAge.class);
         startActivity(i);
+        finish();
 
     }
 
@@ -52,53 +85,60 @@ public class Profile_Preferences extends AppCompatActivity {
 
         Intent i = new Intent(getApplicationContext(),PreferencesDistance.class);
         startActivity(i);
+        finish();
 
     }
 
     public void conncetions(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
     }
 
     public void languages(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
 
     }
 
     public void orientation(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
 
     }
 
     public void ethnicity(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
 
     }
 
     public void politicals(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
-
+        finish();
     }
 
     public void education(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
-
+        finish();
     }
 
     public void employment(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
 
     }
 
     public void Astrlogical_Sign(View view) {
         Intent i = new Intent(getApplicationContext(),EmptyRequiredData.class);
         startActivity(i);
+        finish();
 
     }
 }
